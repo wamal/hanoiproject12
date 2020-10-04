13899f3e523745ccc3afaca03ae21ae67f08a577
wamal <wassimammal@gmail.com>
2020-10-04 10:30:51 +0100

    first commit

---
+++ src/main/java/com/example/demo/Contr.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/hanoi")
+public class Contr {
+	@RequestMapping("/smm")
+	String welcome() {
+		return "index";
+	}
+}
